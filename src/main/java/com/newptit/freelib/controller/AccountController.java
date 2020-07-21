d0f647df0a8fcae052626a2a0948e4d3dd00be53
minhtancnttptit <minhtancnttptit@gmail.com>
2020-07-21 22:11:24 +0700

    fix

--- src/main/java/com/newptit/freelib/controller/AccountController.java
+++ src/main/java/com/newptit/freelib/controller/AccountController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://freelib.minhtancnttptit.vercel.app/")
 @RequestMapping("/api")
 public class AccountController {
     @Autowired
