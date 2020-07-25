580f1a9dca2ef9eb731344857729040a359052b4
minhtancnttptit <minhtancnttptit@gmail.com>
2020-07-25 23:54:07 +0700

    fix

--- src/main/java/com/newptit/freelib/controller/AccountController.java
+++ src/main/java/com/newptit/freelib/controller/AccountController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "https://freelib.vercel.app", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class AccountController {
--- src/main/java/com/newptit/freelib/controller/CommentController.java
+++ src/main/java/com/newptit/freelib/controller/CommentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "https://freelib.vercel.app", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class CommentController {
