e8607ec76aed8fa1bc9d31780bf2dac8ec743bfb
minhtancnttptit <minhtancnttptit@gmail.com>
2020-07-26 02:30:23 +0700

    fix

--- src/main/java/com/newptit/freelib/controller/AdminController.java
+++ src/main/java/com/newptit/freelib/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin(origins = "https://freelib.vercel.app/", maxAge = 3600)
+@CrossOrigin(origins = "https://freelib.vercel.app", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class AdminController {
